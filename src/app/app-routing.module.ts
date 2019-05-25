49e284a9caedc1a7ca30e7017797156cfeb8e5d8
systems-multimedia <samuelboada@gmail.com>
2019-05-25 16:59:43 -0400

    x

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { LicoresComponent } from './components/licores/licores.component';
 import { EmbutidosComponent } from './components/embutidos/embutidos.component';
 import { ProductoComponent } from './components/producto/producto.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: '', component: NavBarComponent, data: { title: 'Wine Store' }, children: [
       { path: 'home', component: HomeComponent, data: { title: 'Home' }, pathMatch: 'full' },
-      { path: 'product/licores', component: LicoresComponent, data: { title: 'Licores' }, pathMatch: 'full' },
-      { path: 'products/embutidos', component: EmbutidosComponent, data: { title: 'Embutidos' } },
+      // { path: 'product/licores', component: LicoresComponent, data: { title: 'Licores' }, pathMatch: 'full' },
+      // { path: 'products/embutidos', component: EmbutidosComponent, data: { title: 'Embutidos' } },
+      { path: 'products/:kind', component: ProductosComponent, data: { title: 'Embutidos' } },
       { path: 'products/:kind/id/:id', component: ProductoComponent, data: { title: ':kind' } }
     ]
   },
