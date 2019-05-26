7da5ba289481ae6bf534df46a0ff6bd5987feb79
systems-multimedia <samuelboada@gmail.com>
2019-05-25 23:18:28 -0400

    sections

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from 'src/app/service/product.service';
+import { Product } from 'src/app/model/product';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-productos',
@@ -7,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductosComponent implements OnInit {
 
-  constructor() { }
+  private section: string;
+  products: Observable<Product[]>;
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) { }
 
   ngOnInit() {
+    this.section = this.route.snapshot.params['kind'];
+    this.products = this.productService.getSection(this.section);
   }
 
 }
